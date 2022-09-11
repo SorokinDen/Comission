9942849caee9c46ad93cef2db482bb9102549f96
Den Sorokin <raptor.perm@gmail.com>
2022-09-11 16:07:37 +0300

    починил сборку

--- src/test/kotlin/TestCommision.kt
+++ src/test/kotlin/TestCommision.kt
@@ -7,7 +7,7 @@ class TestCommision {
         val transfer = 10000
         val sumTransfer = 0
         val result = checkCommissionVKPay(sumTransfer, transfer)
-        assertEquals("", result)
+        assertEquals("Комиссия не взимается", result)
     }
 
     @Test
